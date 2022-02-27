131822332c816fe25d610e5de4506924ec516bdb
KaatClinckemaillie <kaat.clinckemaillie@student.howest.be>
2022-02-27 16:07:24 +0100

    fill svg with random color

--- src/components/Circle.js
+++ src/components/Circle.js
@@ -1,5 +1,6 @@
 import CirclePath from "./CirclePath";
 import PropTypes from "prop-types";
+import { random } from "../functions/lib.js";
 
 const Circle = ({layers}) => {
     const artworkLayers = [
@@ -7,9 +8,12 @@ const Circle = ({layers}) => {
         [{fill: '#99F2EF', rotate:'rotate(20, 50, 50)'} , {fill: '#00FF00', rotate:'rotate(120, 50, 50)'}],
         [{fill: '#99F2EF', rotate:'rotate(20, 50, 50)'}, {fill: '#00FF00', rotate:'rotate(120, 50, 50)'} , {fill: '#0000FF', rotate:'rotate(90, 50, 50)'}]
     ]
+
+    const colors = ['#99F2EF','#00FF00','#0000FF'];
+
     return (
         <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
-            {artworkLayers[layers-1].map((layer, index) => < CirclePath rotate={layer.rotate} fill={layer.fill} key={index}/>)}            
+            {artworkLayers[layers-1].map((layer, index) => < CirclePath rotate={layer.rotate} fill={colors[random(0,colors.length)]} key={index}/>)}            
         </svg>
     )
 }
--- src/functions/lib.js
+++ src/functions/lib.js
@@ -1,4 +1,6 @@
 // directly export the random function instead of a default export
-export const random = (min = 0, max = 1) => {
-    return Math.random() * (max - min) + min;
+export const random = (min, max) => {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min);
 }
